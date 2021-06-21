6c8da931c49f135f24a587fc10faab3af3af57aa
MarkMason1 <71779805+MarkMason1@users.noreply.github.com>
2021-06-21 19:01:57 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B1 = new System.Windows.Forms.Button();
+            this.B4 = new System.Windows.Forms.Button();
+            this.B7 = new System.Windows.Forms.Button();
+            this.B8 = new System.Windows.Forms.Button();
+            this.B5 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B6 = new System.Windows.Forms.Button();
+            this.B9 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(73, 45);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(111, 108);
+            this.B1.TabIndex = 0;
+            this.B1.Text = "button1";
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // B4
+            // 
+            this.B4.Location = new System.Drawing.Point(73, 159);
+            this.B4.Name = "B4";
+            this.B4.Size = new System.Drawing.Size(111, 108);
+            this.B4.TabIndex = 1;
+            this.B4.Text = "button4";
+            this.B4.UseVisualStyleBackColor = true;
+            this.B4.Click += new System.EventHandler(this.B4_Click);
+            // 
+            // B7
+            // 
+            this.B7.Location = new System.Drawing.Point(73, 273);
+            this.B7.Name = "B7";
+            this.B7.Size = new System.Drawing.Size(111, 108);
+            this.B7.TabIndex = 2;
+            this.B7.Text = "button7";
+            this.B7.UseVisualStyleBackColor = true;
+            this.B7.Click += new System.EventHandler(this.B7_Click);
+            // 
+            // B8
+            // 
+            this.B8.Location = new System.Drawing.Point(190, 273);
+            this.B8.Name = "B8";
+            this.B8.Size = new System.Drawing.Size(111, 108);
+            this.B8.TabIndex = 3;
+            this.B8.Text = "button8";
+            this.B8.UseVisualStyleBackColor = true;
+            this.B8.Click += new System.EventHandler(this.B8_Click);
+            // 
+            // B5
+            // 
+            this.B5.Location = new System.Drawing.Point(190, 159);
+            this.B5.Name = "B5";
+            this.B5.Size = new System.Drawing.Size(111, 108);
+            this.B5.TabIndex = 4;
+            this.B5.Text = "button5";
+            this.B5.UseVisualStyleBackColor = true;
+            this.B5.Click += new System.EventHandler(this.B5_Click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(190, 45);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(111, 108);
+            this.B2.TabIndex = 5;
+            this.B2.Text = "button2";
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.B2_Click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(307, 45);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(111, 108);
+            this.B3.TabIndex = 6;
+            this.B3.Text = "button3";
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // B6
+            // 
+            this.B6.Location = new System.Drawing.Point(307, 159);
+            this.B6.Name = "B6";
+            this.B6.Size = new System.Drawing.Size(111, 108);
+            this.B6.TabIndex = 7;
+            this.B6.Text = "button6";
+            this.B6.UseVisualStyleBackColor = true;
+            this.B6.Click += new System.EventHandler(this.B6_Click);
+            // 
+            // B9
+            // 
+            this.B9.Location = new System.Drawing.Point(307, 273);
+            this.B9.Name = "B9";
+            this.B9.Size = new System.Drawing.Size(111, 108);
+            this.B9.TabIndex = 8;
+            this.B9.Text = "button9";
+            this.B9.UseVisualStyleBackColor = true;
+            this.B9.Click += new System.EventHandler(this.B9_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(345, 59);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Restart Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 520);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.B9);
+            this.Controls.Add(this.B6);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B5);
+            this.Controls.Add(this.B8);
+            this.Controls.Add(this.B7);
+            this.Controls.Add(this.B4);
+            this.Controls.Add(this.B1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B4;
+        private System.Windows.Forms.Button B7;
+        private System.Windows.Forms.Button B8;
+        private System.Windows.Forms.Button B5;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B6;
+        private System.Windows.Forms.Button B9;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int turn = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+       
+        public void Turn()
+        {
+            if (turn% 2 == 0)
+            {
+                Text = $"Turn: player: 1(X)";
+                
+            }
+            else
+            {
+                Text = $"Turn: player: 2(O)";
+                
+
+            }
+        }
+        public void check()
+        {
+            Turn();
+            if ((B1.Text == B2.Text && B2.Text == B3.Text) 
+                || (B1.Text == B5.Text && B5.Text == B9.Text) 
+                || (B1.Text == B4.Text && B7.Text == B4.Text))
+            {
+                if (turn % 2 == 0)
+                {
+                    MessageBox.Show("Player 2 WINS!!!!");
+                    Application.Restart();
+                }
+                else
+                {
+                    MessageBox.Show("Player 1 WINS!!!!");
+                    Application.Restart();
+                }
+                
+
+            }
+            
+        }
+        private void B1_Click(object sender, EventArgs e)
+        {
+            if(turn % 2 != 0)
+            {
+                B1.Text = "X";
+                B1.Enabled = false;
+
+                check();
+            }
+            else
+            {
+                B1.Text = "O";
+                B1.Enabled = false;
+                check();
+
+            }
+            turn++;
+        }
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B2.Text = "X";
+                B2.Enabled = false;
+                check();
+
+            }
+            else
+            {
+                B2.Text = "O";
+                B2.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B3.Text = "X";
+                B3.Enabled = false;
+                check();
+            }
+            else
+            {
+                B3.Text = "O";
+                B3.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void B4_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B4.Text = "X";
+                B4.Enabled = false;
+                check();
+            }
+            else
+            {
+                B4.Text = "O";
+                B4.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void B5_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B5.Text = "X";
+                B5.Enabled = false;
+                check();
+            }
+            else
+            {
+                B5.Text = "O";
+                B5.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void B6_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B6.Text = "X";
+                B6.Enabled = false;
+                check();
+            }
+            else
+            {
+                B6.Text = "O";
+                B6.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void B7_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B7.Text = "X";
+                B7.Enabled = false;
+                check();
+            }
+            else
+            {
+                B7.Text = "O";
+                B7.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void B8_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B8.Text = "X";
+                B8.Enabled = false;
+                check();
+            }
+            else
+            {
+                B8.Text = "O";
+                B8.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void B9_Click(object sender, EventArgs e)
+        {
+            if (turn % 2 != 0)
+            {
+                B9.Text = "X";
+                B9.Enabled = false;
+                check();
+            }
+            else
+            {
+                B9.Text = "O";
+                B9.Enabled = false;
+                check();
+            }
+            turn++;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
